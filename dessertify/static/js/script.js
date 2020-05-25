fec659d52fc2e4833348ef57f3e4e361be378150
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2020-05-25 23:39:37 +0530

    :sparkles: feat: Complete home page

--- dessertify/static/js/script.js
+++ dessertify/static/js/script.js
@@ -5,15 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var slider = M.Sidenav.init(elems);
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var carousels = M.Carousel.init(elems, {fullWidth:true, indicators:true, duration:200});
-    elems.next();
-
-  });
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
-    elems.next();
-
-  });
-
+    var carousels = M.Carousel.init(elems, {indicators:true, duration:200});
+});
